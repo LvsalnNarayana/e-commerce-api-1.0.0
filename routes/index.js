7d6d575fc580758c3ddb377a4f834985c140a1ae
LvsalnNarayana <lvsalnnarayana@gmail.com>
2022-11-21 23:35:31 +0000

    Api Test - 1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.status(200).send("Hello world...!");
+  res.status(200).json({"test":"Hello world...!"});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.status(200).send("users");
+  res.status(200).json({"users" : "tom"});
 });
 
 module.exports = router;
